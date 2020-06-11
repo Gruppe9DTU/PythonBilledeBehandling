2597e089ffa9c920b90f00a7a54aa7ab09343e8a
s185114 <s185114@student.dtu.dk>
2020-06-11 13:08:13 +0200

    Initial commit

---
+++ Server.py
@@ -0,0 +1,47 @@
+import os, time, threading, socket
+import numpy as np
+import cv2
+
+HOST = '192.168.0.18'
+PORT = 8888
+
+def getImage():
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print("Waiting for connection")
+        conn,addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                newData = conn.recv(1024).decode()
+                if not newData is None and newData != '':
+                    imageReq(newData)
+
+
+
+def imageReq(img):
+    global cascade
+    c = cascade.detectMultiScale(img)
+    
+    for (x,y,w,h) in c:
+        print("tal")
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.imshow('img',img)
+    
+    while 1:
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+
+def initClassifier():
+    global cascade
+    cascade = cv2.CascadeClassifier('cascade.xml')
+
+
+
+
+if __name__ == "__main__":
+    initClassifier
+    getImage
