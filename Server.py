5d5e4333d65d9dc462f0feab7c3e685c517f32b7
Anders <s185100@student.dtu.dk>
2020-06-15 10:24:08 +0200

    Should recieve all bytes from client

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-import os, time, threading, socket, cv2
+import time, threading, socket, cv2, numpy as np
 
 HOST = '192.168.0.51'
 PORT = 8888
@@ -9,13 +9,34 @@ def getImage():
         s.listen()
         print("Waiting for connection")
         conn,addr = s.accept()
+        newData = bytearray()
         with conn:
             print('Connected by', addr)
             while True:
-                newData = conn.recv(1024).decode()
-                if not newData is None and newData != '':
-                    imageReq(newData)
+                sizeOfData = conn.recv(1024)
+                #sizeOfData = int.from_bytes(sizeOfData,byteorder='big')
+                #print("Indhold: "+sizeOfData)
+                while len(newData) < int(sizeOfData):
+                    packet = conn.recv(int(sizeOfData) - len(newData))
+                    if not packet:
+                        continue
+                    newData.extend(packet)
+                #newData = conn.recv(48000000)
+                #mat = np.array(newData)
+                #mat=list(newData)
+                for x in range(20):
+                    print(newData[x])
 
+                
+                nparr = np.frombuffer(newData, np.uint8)#.reshape(3000, 4000)
+                #print(nparr.size)
+                mat = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                imS = cv2.resize(mat, (960, 540))
+                
+                if not (np.array_equal(mat,None)):
+                #if not mat is None and mat != '':
+                    cv2.imshow("Test",imS)
+                    imageReq(mat)
 
 
 def imageReq(img):
@@ -44,5 +65,3 @@ if __name__ == "__main__":
     initClassifier()
     while(True):
         getImage()
-    
-    
