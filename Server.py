67ef0376a4ccc2695cb1c55ad97e85107dc2d39e
Anders <s185100@student.dtu.dk>
2020-06-18 17:25:39 +0200

    Instead of loading all xml files to memory we load one at a time to reduce memory use. Also changed the string containing the image to be decoded to be single channel grayscale with an image size reduced 1/8.

--- Server.py
+++ Server.py
@@ -2,12 +2,11 @@ import socket, cv2, numpy as np
 import cardDetect as cd
 
 HOST = '192.168.0.51'
-PORT = 8889
+PORT = 8888
 
 def getImage(conn):
     newData = bytearray()
     print("Waiting for data...")
-    sizeOfData = None
     sizeOfData = conn.recv(1024)
     if sizeOfData:
         while len(newData) < int(sizeOfData):
@@ -15,11 +14,10 @@ def getImage(conn):
             if not packet:
                 continue
             newData.extend(packet)
-            
+        
         nparr = np.frombuffer(newData, np.uint8)
-
-        mat = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        imS = cv2.resize(mat, (960, 540))
+        
+        mat = cv2.imdecode(nparr, cv2.IMREAD_REDUCED_GRAYSCALE_8)
         
         if not (np.array_equal(mat,None)):
             result = cd.find(mat)
@@ -29,32 +27,12 @@ def getImage(conn):
                 conn.send(test.encode('utf-8'))
                 print(test)
             else:
+                print("Nothing found")
                 conn.send("[]\n".encode('utf-8'))
-            
+
     else:
         print("Connection lost")
         exit()
-
-
-def imageReq(img):
-    global cascade
-    c = cascade.detectMultiScale(img,1.01)
-    for (x,y,w,h) in c:
-        print("tal")
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-        result = cv2.resize(img, (960, 540))
-        cv2.imshow('img',result)
-    
-    while 1:
-        if cv2.waitKey(0) & 0xFF == ord('q'):
-            break
-    
-    return np.concatenate(c, axis=None)
-
-
-def initClassifier():
-    global cascade
-    cascade = cv2.CascadeClassifier('cascade_6.xml')
 
 
 if __name__ == "__main__":
--- cardDetect.py
+++ cardDetect.py
@@ -1,18 +1,12 @@
 import cv2
 
 #Init lists of classifiers
-numlist = []
 typelist = []
-for i in range(1,14):
-    cas = cv2.CascadeClassifier('Cascade/'+str(i)+'.xml')
-    if not cas.empty(): numlist.append([cas, i])
-
 
 typelist.append([cv2.CascadeClassifier('Cascade/h.xml'), 1])
 typelist.append([cv2.CascadeClassifier('Cascade/k.xml'), 2])
 typelist.append([cv2.CascadeClassifier('Cascade/s.xml'), 3])
 typelist.append([cv2.CascadeClassifier('Cascade/r.xml'), 4])
-
 
 
 #Find type of card
@@ -28,25 +22,29 @@ def findtype(img):
 #Find card and value
 def find(img):
     result=[]
-    for (cas,i) in numlist:
+    for i in range(1,14):
+        cas = cv2.CascadeClassifier('Cascade/'+str(i)+'.xml')
+        if cas.empty(): 
+            continue
         c=cas.detectMultiScale(img,1.01)
         for (x,y,w,h) in c:
             img2 = img[y+h//2:y+h*2,x:x+w]          #Cut a piece of the card
             t=findtype(img2)                        #Find type for the given value
             #print(t)
             if t!=0: result.append([x,y,w,h,i,t])   #If type, then we have a card
+    cas = None
     return result
 
 
 #Test
 
-if __name__ == "__main__":
-    img=cv2.imread("1.jpg")
-    x,y,z = img.shape
-    z = y/500
-    img = cv2.resize(img,(int(y/z),int(x/z)))
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    r=find(gray)
-    print(r)
-    print(len(r))
+#if __name__ == "__main__":
+#    img=cv2.imread("1.jpg")
+#    x,y,z = img.shape
+#    z = y/500
+#    img = cv2.resize(img,(int(y/z),int(x/z)))
+#    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#    r=find(gray)
+#    print(r)
+#    print(len(r))
     #'''
